5b1c834619eb4a756336817fb0d8e6d5f8bdfd83
Kakaluoto <1052176873@qq.com>
2020-12-14 14:12:04 +0800

    v1.00

--- myblog/settings.py
+++ myblog/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainsite'
+    'mainsite',
+    'markdown_deux'
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]

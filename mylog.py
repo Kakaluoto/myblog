aa8f25da84d62fe38269b884d6913711bf041b40
Kakaluoto <1052176873@qq.com>
2020-12-20 15:41:28 +0800

    v1.201

--- mylog.py
+++ mylog.py
@@ -1,3 +1,4 @@
+print("""
 print('hello world')
 print('这次测试git push是否还需要输入账号密码')
 print('这次测试自动push的shell脚本')
@@ -5,3 +6,4 @@ print('pycharm连接出错了，不知道好了没')
 print('我再试一下，还是不行啊')
 print('好像终于可以了,害')
 操啊，github又上传不了了
+""")

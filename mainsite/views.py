c71326b242213be2df456a6697e5638c8559de6d
Kakaluoto <1052176873@qq.com>
2020-12-20 09:27:25 +0800

    v1.11

--- mainsite/views.py
+++ mainsite/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import redirect
 from .models import Post, Product
 from datetime import datetime
+import random
 
 
 # Create your views here.
@@ -23,60 +24,24 @@ def showpost(request, slug):
 
 
 def about(request):
-    return render(request, 'about.html')
+    quotes = [
+        '知识不求人。',
+        '己所不欲，勿施于人。',
+        '没有调查就没有发言权。',
+        '我之所以想变强，是为了活得轻松写意。'
+    ]
+    quote = random.choice(quotes)
+    return render(request, 'about.html', locals())
 
 
 def listing(request):
-    html = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset='uft-8'>
-        <title>
-            货物列表
-        </title>
-    </head>
-    <body>
-        <h2>当前存货列表</h2>
-        <hr>
-        <table width=400 border=1 bgcolor='#ccffcc'>
-        {}
-        </table>
-    </body>
-    </html>
-    '''
     products = Product.objects.all()
-    tags = '<tr><td>产品</td><td>售价</td><td>库存量</td></tr>'
-    for p in products:
-        tags = tags + '<tr><td>{}</td>'.format(p.name)
-        tags = tags + '<td>{}</td>'.format(p.price)
-        tags = tags + '<td>{}</td></tr>'.format(p.restp)
-    return HttpResponse(html.format(tags))
+    return render(request, 'list.html', locals())
 
 
 def disp_detail(request, sku):
-    html = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset='uft-8'>
-        <title>{}</title>
-    </head>
-    <body>
-        <h2>{}</h2>
-        <hr>
-        <table width=400 border=1 bgcolor='#ccffcc'>
-        {}
-        </table>
-        <a href='/list'>返回列表</a>
-    </body>
-    </html>
-    '''
     try:
         p = Product.objects.get(sku=sku)
     except Product.DoesNotExist:
         raise Http404('找不到指定的产品编号')
-    tags = '<tr><td>产品编号</td><td>{}</td></tr>'.format(p.sku)
-    tags = tags + '<tr><td>产品名称</td><td>{}</td></tr>'.format(p.name)
-    tags = tags + '<tr><td>库存数量</td><td>{}</td></tr>'.format(p.restp)
-    return HttpResponse(html.format(p.name, p.name, tags))
+    return render(request, 'disp.html', locals())
